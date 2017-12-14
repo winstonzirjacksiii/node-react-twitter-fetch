2656c551e92af94f29ead4cf5e9b9c7c28aa1281
winstonzirjacksiii <winston.zirjacks.iii+github@gmail.com>
2017-12-13 22:39:12 -0800

    added a searchbar component and restructured things

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from './SearchBar';
+import '../css/App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { tweets: [] };
+    this.state = {
+      tweets: []
+    };
   }
 
-  componentDidMount(){
-    fetch('/search/cats')
+  searchForTweets(term){
+    fetch('/search/'+ term)
     .then((res) => res.json())
     .then((resObj) => {
       this.setState({ tweets: resObj.statuses });
@@ -20,8 +22,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Here are some tweets about cats</h1>
+          <h1 className="App-title">Tweet Lookup</h1>
+          <h3 className="App-subtitle">using React, Node, and Firebase</h3>
         </header>
+        <SearchBar searchFunc={this.searchForTweets.bind(this)} />
+        
         <section className="Tweets">
           { this.state.tweets.map((tweet, i) => <div key={i}>{tweet.text}</div>) }
         </section>
---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../css/SearchBar.css';
+
+class SearchBar extends Component {
+  constructor(props){
+    super(props);    
+    this.state = {
+      term: ""
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    debugger;
+    this.props.searchFunc(this.state.term);
+  }
+
+  render() {
+    return (
+      <form className="m-searchBar" onSubmit={this.handleSubmit}>
+        <input className="m-searchBar--input" 
+               placeholder="Search for tweets..." 
+               onChange={(event)=>{this.setState({ term: event.target.value })}} />
+      </form>
+    )
+  }
+
+}
+
+export default SearchBar;
